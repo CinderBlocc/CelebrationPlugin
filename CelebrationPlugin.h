27579aafbff66d978a7d3f3bdd4d6a3d803e68e2
CinderBlocc <cinder.block.rl@gmail.com>
2020-12-19 14:47:58 -0600

    Linked against PluginSDK.lib

--- CelebrationPlugin.h
+++ CelebrationPlugin.h
@@ -1,5 +1,5 @@
 #pragma once
-#pragma comment(lib, "BakkesMod.lib")
+#pragma comment(lib, "PluginSDK.lib")
 #include "bakkesmod/plugin/bakkesmodplugin.h"
 
 class CelebrationPlugin : public BakkesMod::Plugin::BakkesModPlugin
